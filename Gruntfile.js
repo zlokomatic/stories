577a156343ab1af920648c1ecf18724b02eb26bb
zlokomatic <zlokomatic@gmail.com>
2015-11-16 23:15:47 +0100

    Initial setup

---
+++ Gruntfile.js
@@ -0,0 +1,63 @@
+module.exports = function(grunt) {
+  require("load-grunt-tasks")(grunt); // npm install --save-dev load-grunt-tasks
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    browserify: {
+      dist: {
+        options: {
+          transform: [
+            ["babelify", {
+              sourceMaps: 'inline'
+            }]
+          ]
+        },
+        files: {
+          "./dist/js/index.js": ["./src/index.js"]
+        }
+      }
+    },
+    watch: {
+      scripts: {
+        files: ["./src/**"],
+        tasks: ['copy', 'sass', 'browserify']
+      }
+    },
+    browserSync: {
+      bsFiles: {
+        src : 'dist/**'
+      },
+      options: {
+        server: {
+          baseDir: "./dist"
+        },
+        watchTask: true
+      }
+    },
+    copy: {
+      index: {
+        src: './src/index.html',
+        dest: './dist/index.html'
+      },
+      bootstrapjs: {
+        src: './node_modules/bootstrap-sass/assets/javascripts/bootstrap.min.js',
+        dest: './dist/js/bootstrap.min.js'
+      }
+    },
+    sass: {
+      options: {
+        sourceMap: true,
+        includePaths: ['./src/styles/', './node_modules/bootstrap-sass/assets/stylesheets/']
+      },
+      dist: {
+        files: {
+          './dist/css/main.css': './src/styles/index.scss'
+        }
+      }
+    }
+  });
+
+  // Default task(s).
+  grunt.registerTask('default', ['copy', 'sass', 'browserify', 'browserSync', 'watch']);
+
+};
